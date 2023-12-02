c6dd0b1ddc304ed07f99b29233bcf66c475e1f45
jawad35 <jaqimughal@gmail.com>
2023-12-01 21:49:48 -0800

    added many pages

---
+++ src/pages/portfolio.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Portfolio from '../components/Portfolio';
+import SEO from '../components/seo';
+
+const PortfolioFive = () => {
+  return (
+    <>
+      <SEO pageTitle="Portfolio Five" />
+      <Portfolio/>
+    </>
+  );
+};
+
+export default PortfolioFive;
